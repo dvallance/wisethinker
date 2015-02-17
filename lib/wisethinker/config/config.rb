5c66940b5d671458818f31f8865d923d1f6e5b67
David Vallance <davevallance@gmail.com>
2015-02-17 13:39:30 -0800

    default storage accessor to localhost

--- lib/wisethinker/config/config.rb
+++ lib/wisethinker/config/config.rb
@@ -1,15 +1,10 @@
 module Wisethinker
   module Config
 
-    storage_accessor_host = ENV['STORAGE_ACCESSOR_HOST']
+    storage_accessor_host = ENV['STORAGE_ACCESSOR_HOST'] || 'localhost'
 
     #Currently I have no plans to deviate from the default ports
     #storage_accessor_port = ENV['STORAGE_ACCESSOR_PORT']
-
-    #I use docker containers even in development so I have no safe 'localhost'
-    #fallback. Always demand a host!
-    raise "Need to set environment variable [STORAGE_ACCESSOR_HOST]" unless ENV['STORAGE_ACCESSOR_HOST']
-
 
     case ENV['RACK_ENV']
     when 'production'
@@ -19,7 +14,6 @@ module Wisethinker
     when 'test'
 
     end
-
 
     DOCUMENT_ACCESSOR = DocumentAccessor.new(
       StorageAccessor::Rethink.new(host: storage_accessor_host)
